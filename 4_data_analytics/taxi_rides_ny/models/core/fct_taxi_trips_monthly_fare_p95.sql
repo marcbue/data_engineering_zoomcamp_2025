f30dff5ddab2b1fed78cbbaa1bd1601d963a07d1
Marc Büttner <marc.buettner89@gmail.com>
2025-02-23 18:26:08 +0000

    Homework 6

---
+++ 4_data_analytics/taxi_rides_ny/models/core/fct_taxi_trips_monthly_fare_p95.sql
@@ -0,0 +1,34 @@
+{{ config(materialized='table') }}
+
+with trips_data as (
+    select * from {{ ref('fact_trips') }}
+),
+filtered_trips as (
+    select *
+    from trips_data
+    where fare_amount > 0
+      and trip_distance > 0
+      and payment_type_description in ('Cash', 'Credit card')
+),
+monthly_percentiles as (
+    select 
+        service_type,
+        pickup_year,
+        pickup_month,
+        APPROX_QUANTILES(fare_amount, 100)[OFFSET(97)] as p97,
+        APPROX_QUANTILES(fare_amount, 100)[OFFSET(95)] as p95,
+        APPROX_QUANTILES(fare_amount, 100)[OFFSET(90)] as p90
+    from filtered_trips
+    where pickup_year = 2020
+        and pickup_month = 4
+    group by service_type, pickup_year, pickup_month
+)
+
+
+select *
+from monthly_percentiles
+-- select *
+-- from monthly_percentiles
+-- where pickup_year = 2020
+--   and pickup_month = 4
+--   and service_type in ('Green Taxi', 'Yellow Taxi')
